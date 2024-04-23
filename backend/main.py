c6f04cb0e3ad00c4632de52d034d41c687b246df
graham-livingston <glivingston@uchicago.edu>
2024-04-22 19:47:33 -0500

    added exit route and error handeling

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from modules import Reservation, ReservationCalendar, DateRange
 
 app = FastAPI()
@@ -9,3 +9,16 @@ calendar = ReservationCalendar()
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/exit/{persist_status}")
+def exit_handler(persist_status: bool = False):
+    try:
+        if persist_status:
+            calendar.save_reservations()
+            return {"message": "Your changes have been saved. Goodbye!"}
+        else: 
+            return {"message": "Goodbye!"}
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f'Failed to save changes due to {e}')
