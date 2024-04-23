c0967b51a86c326b4f479c886a4f8152d3e2b0d4
graham-livingston <glivingston@uchicago.edu>
2024-04-22 19:57:57 -0500

    added optional query parameter and error handeling

--- backend/main.py
+++ backend/main.py
@@ -10,14 +10,17 @@ calendar = ReservationCalendar()
 def root():
     return {"message": "Hello World"}
 
-@app.get("/exit/{persist_status}")
+@app.get("/exit/")
 def exit_handler(persist_status: bool = False):
     try:
-        if persist_status:
+        if persist_status is True:
             calendar.save_reservations()
             return {"message": "Your changes have been saved. Goodbye!"}
-        else: 
+        elif persist_status is False: 
             return {"message": "Goodbye!"}
+        else:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Invalid value for persist_status. Please provide a boolean value.")
 
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
