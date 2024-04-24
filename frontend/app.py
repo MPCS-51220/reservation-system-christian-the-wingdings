f20ed6a71560126622eb56ca57bc58abdd1a56a2
Akshatha Ramkumar <akshatha.0712@gmail.com>
2024-04-24 11:13:12 -0500

    changed client code

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,13 @@
 import requests
 import json
 
-BASE_URL = "http://localhost:8000/"
+BASE_URL = "http://localhost:8000"
 
 def get_machine_choice():
-    print("\n1. Multi-phasic radiation scanner\n2. Ore scooper\n3. 1.21 gigawatt lightning harvester")
+    """
+    This function gets the user's choice of machine
+    """
+    print("\n\n1. Multi-phasic radiation scanner\n2. Ore scooper\n3. 1.21 gigawatt lightning harvester\n\n")
     choice = input("\nEnter your machine choice: ")
     if choice == '1':
         machine = "scanner"
@@ -19,47 +22,52 @@ def get_machine_choice():
 
 def make_reservation():
     try:
-        customer_name = input("Enter customer name")
+        customer_name = input("\nEnter customer name: ")
         machine = get_machine_choice()
         if not machine:
             return    
-        start_date = input("Enter start date (YYYY-MM-DD): ")
+        start_date = input("\nEnter start date (YYYY-MM-DD): ")
+        end_date = input("\nEnter end date (YYYY-MM-DD): ")
+
         data ={
             "customer": customer_name,
             "machine": machine,
-            "start_date": start_date
-        }
+            "start_date": start_date,
+            "end_date": end_date
+        } # post request body
         
         response = requests.post(f"{BASE_URL}/reservations", json=data)
         if response.status_code == 201:
-            print("Reservation added successfully")
+            print("\nReservation added successfully")
         else:
-            print("Failed to add reservation")
+            print(response.json()['detail']) # print HTTPException message
     except Exception as e:
-        print("Error while adding reservation ", str(e))
+        print("Error occurred while adding reservation ", str(e))
 
 def cancel_reservation():
     try:
-        reservation_id = input("Enter id of the reservation to cancel")
+        reservation_id = input("\nEnter id of the reservation to cancel: ")
         response = requests.delete(f"{BASE_URL}/reservations/{reservation_id}")
         if response.status_code == 200:
             refund = response.json()['refund']
-            print("Reservation cancelled successfully\nRefund amount: ", refund)
+            print("\nReservation cancelled successfully\nRefund amount: ", refund)
+        else:
+            print(response.json()['detail'])
     except Exception as e:
-        print("Reservation not found")
+        print("An error occurred ", str(e))
 
 
 def list_reservations():
-    print("1. List reservations for a given date range\n\
-          2. List the reservations for a given machine for a given date range\n\
-          3. List the reservations for a given customer for a given date range")
+    print("\n\n1. List reservations for a given date range\n")
+    print("2. List the reservations for a given machine for a given date range\n")
+    print("3. List the reservations for a given customer for a given date range\n\n")
     choice = input("\nEnter your choice: ")
     start_date = input("\nEnter start date (YYYY-MM-DD)  :")
     end_date = input("\nEnter end date (YYYY-MM-DD)  :")
     params = {
         "start": start_date,
         "end": end_date
-    }
+    } # url paramaters
 
     try: 
         if choice == '1':
@@ -72,40 +80,46 @@ def list_reservations():
             response = requests.get(f"{BASE_URL}/reservations/machines/{machine}", params=params)
 
         elif choice == '3':
-            customer = input("Enter customer name: ")
+            customer = input("\nEnter customer name: ")
             response = requests.get(f"{BASE_URL}/reservations/customers/{customer}", params=params)
 
         else:
             print("Invalid choice. Back to the main menu")
             return
-        
-        if 'reservations' in response:
-            print(response['reservations'])
+
+        if response.status_code == 200:
+            if 'reservations' in response: # reservations have been returned
+                print(response.json()['reservations'])
+            else: # no reservations found
+                print(response.json()['message'])
+
         else:
-            print(response['message'])
+            print(response.json()['detail'])
 
     except Exception as e:
         print("Error occurred in retrieving reservations ",str(e))
 
 def exit_system():
     try:
-        choice = input("Do you want to save your changes? Y/N")
+        choice = input("\nDo you want to save your changes? Y/N:   ")
         if choice.lower() == 'y':
             params={'persist_status':True}
         else:
             params={'persist_status':False}
-        requests.get(f"{BASE_URL}/exit",params=params)
+        response = requests.get(f"{BASE_URL}/exit",params=params)
+        if response.status_code == 200:
+            print(response.json()['message']) # success message
+        else:
+            print(response.json()['detail']) # printing HTTPException message
+
     except Exception as e:
         print("Error while saving changes ",str(e))
 
 def main():
     while True:
-        print("Welcome to the Reservation System!")
-        print("\n1. Make reservation\n\
-              2. Cancel reservation\n\
-              3. List reservations\n\
-              4. Quit")
-        choice = input("Enter your choice")
+        print("\nWelcome to the Reservation System!\n")
+        print("\n\n1. Make reservation\n2. Cancel reservation\n3. List reservations\n4. Quit\n\n")
+        choice = input("\nEnter your choice: ")
         if choice == '1':
             make_reservation()
         elif choice == '2':
